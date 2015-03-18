7bb88f9dee015838978247f9c3bd1822e977d112
lior@247 <lior@linnovate.net>
2015-03-18 11:56:51 -0400

    adding duplicator

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '247lock');
+define('DB_NAME', '247lockdoc');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'noam');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'noam11');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
